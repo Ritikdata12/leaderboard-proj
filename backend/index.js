1b03e2deb13e59abe3fc97930d6a1bd98dff1e94
Ritikdata12 <ritikdata123@gmail.com>
2024-10-15 22:50:51 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const multer = require('multer'); 
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require("./routes/userRoutes");
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api', userRoutes)
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
