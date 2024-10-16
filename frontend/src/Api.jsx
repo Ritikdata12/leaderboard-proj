9176cad8705ddd97a99084ce8bf326c0cd1ecd9c
Ritikdata12 <ritikdata123@gmail.com>
2024-10-16 11:05:12 +0530

    last commit

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://leaderboard-proj-6u9j.vercel.app/';
+const API_URL = 'https://leaderboard-proj.vercel.app/';
 
 export const getUsers = async () => {
   const res = await axios.get(`${API_URL}/users`);
